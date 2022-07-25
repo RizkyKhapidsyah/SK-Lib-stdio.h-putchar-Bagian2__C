2dd518884f6e8d665cabf921d9c3881d5a3f7c2d
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-07-25 21:01:27 +0700

    Initial commit

---
+++ SK-Lib-stdio.h-putchar-Bagian2__C/Source.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <conio.h>
+
+/*
+    Source by GeeksForGeeks (https://www.geeksforgeeks.org)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main() {
+    char ch = 'G';
+    
+    putchar(ch);
+
+    _getch();
+    return (0);
+}
